b8f948dce53eef88d5de5d8f4ee4904dd4b94a8e
Azim Momin <ayyem@protonmail.com>
2024-03-26 19:32:30 -0400

    initial commit.

---
+++ 01-content-projection/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from "@angular/core";
+
+import { User } from "./auth-form/auth-form.interface";
+
+@Component({
+  selector: "app-root",
+  template: `
+    <div>
+      <auth-form (submitted)="createUser($event)">
+        <h3>Create account</h3>
+        <button type="submit">Join Us</button>
+      </auth-form>
+      <auth-form (submitted)="loginUser($event)">
+        <h3>Login</h3>
+        <button type="submit">Login</button>
+        <auth-remember (checked)="rememberUser($event)"></auth-remember>
+      </auth-form>
+    </div>
+  `,
+})
+export class AppComponent {
+  rememberMe: boolean = false;
+
+  createUser(user: User): void {
+    console.log("Create account", user);
+  }
+
+  loginUser(user: User): void {
+    console.log("Login", user, this.rememberMe);
+  }
+
+  rememberUser(remember: boolean): void {
+    this.rememberMe = remember;
+  }
+}
